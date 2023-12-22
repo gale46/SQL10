bc08381a1f067b88dafc31a2bc15ec012d9c44de
gale46 <113490808+gale46@users.noreply.github.com>
2023-12-22 15:39:54 +0800

    Update 10-3-view2.sql

--- 10-3-view2.sql
+++ 10-3-view2.sql
@@ -2,6 +2,6 @@
 go
 create view Em_view
 as 
-select A.Em_id,A.Em_name,A.Em_code
+select A.Em_id,A.Em_name,A.Comp_dep_name
 from [dbo].[Employee] as A, [dbo].[Comp_dep] as B
-where A.Em_code = B.Comp_dep_code
+where A.Em_code = B.Comp_dep_name
