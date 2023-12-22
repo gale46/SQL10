40c0a801cdf57bc61ae174a74e8250a0a6ada6aa
gale46 <113490808+gale46@users.noreply.github.com>
2023-12-22 15:59:14 +0800

    Update 10-5-2_ex01.sql

--- 10-5-2_ex01.sql
+++ 10-5-2_ex01.sql
@@ -1,6 +1,6 @@
 use MyDBMS
 go 
-create view db前三名成績單 as
+create view db_top3 as
 select top 3 stu_name, stu_course, stu_grade
 from Student as A, Selection as B, Course as C
 where A.stu_id = B.s_id
